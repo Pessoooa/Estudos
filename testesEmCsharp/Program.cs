1c72976a2e566c394d6fea2c512b43cbe80f3330
Pessoa <williampessowa@gmail.com>
2024-10-07 00:09:04 -0300

    Somadora de dois numeros.

---
+++ testesEmCsharp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite um numero: ");
+var n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite outro numero: ");
+var n2 = int.Parse(Console.ReadLine());
+var soma = n1 + n2;
+Console.WriteLine("A soma de " + n1 + " e " + n2 + " é: " + soma );
