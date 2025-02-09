2444184ea9764a864cfafd4dd6b0627daaa067e9
William Pessoa <williampessowa@gmail.com>
2025-02-09 20:16:28 -0300

    Projeto Balta de OOP

---
+++ Balta/Balta/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Balta.ContentContext;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+            articles.Add(new Article("Artigo sobre C#", "csharp"));
+            articles.Add(new Article("Artigo sobre Dotnet", "dotnet"));
+
+            // Descomente o loop abaixo se quiser imprimir os artigos
+            // foreach (var article in articles)
+            // {
+            //     Console.WriteLine(article.Id);
+            //     Console.WriteLine(article.Title);
+            //     Console.WriteLine(article.Url);
+            // }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fundamentos DotNet", "fundamentos-dotnet");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem3 = new CareerItem(2, "OOP", "", courseOOP); // Corrigido para não ser null
+            var careerItem = new CareerItem(1, "Comece por aqui", "", courseCsharp);
+            var careerItem2 = new CareerItem(3, "Aprenda .NET", "", courseAspNet);
+            careerDotnet.Items.Add(careerItem);
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem3);
+            careers.Add(careerDotnet);
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                    if (item.Course != null) // Verifica se o curso não é nulo
+                    {
+                        Console.WriteLine($"Curso: {item.Course.Title}");
+                    }
+                }
+            }
+        }
+    }
+}
